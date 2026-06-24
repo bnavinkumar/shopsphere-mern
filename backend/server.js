de430bff65c960edba7c8ec709b9e57e91fc7683
Navin B <nav77318@gmail.com>
2026-06-25 01:17:27 +0530

    Fix server cors

--- backend/server.js
+++ backend/server.js
@@ -14,15 +14,11 @@ import analyticsRoutes from "./routes/analytics.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
-console.log("ENV TEST =", process.env.MONGO_URI);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
-app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
-app.use(cookieParser());
+// CORS CONFIG
 app.use(
 	cors({
 		origin: [
@@ -33,6 +29,11 @@ app.use(
 	})
 );
 
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
@@ -40,15 +41,7 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-//if (process.env.NODE_ENV === "production") {
-//	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-//
-//	app.get("*", (req, res) => {
-//		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//	});
-//}
-
 app.listen(PORT, () => {
-	console.log("Server is running on http://localhost:" + PORT);
+	console.log(`Server running on port ${PORT}`);
 	connectDB();
 });
